e19a781ea59b255dfe86f0fc3c6b89dfdaca0676
Simon Mulquin <simon.mulquin@gmail.com>
2024-11-13 22:44:00 +0900

    style: ♻️ Makes demand card a component

---
+++ app/src/lib/Components/DemandCard.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	let { demand, number }: { demand: {title: string; id: string}, number: number } = $props();
+
+</script>
+
+<li>
+	<div>
+		<h3>Demand {number}</h3>
+		<p>{demand.title}</p>
+	</div>
+	<div class="li_footer">
+		<img alt="A placeholder" src="/placeholder.gif" />
+		<a href="/{demand.id}">check it out ↗</a>
+	</div>
+</li>
+
+<style>
+	li {
+		display: inline-block;
+		width: 30%;
+		border-radius: var(--spacing-3);
+		padding: var(--spacing-4) var(--spacing-4) 18% var(--spacing-4);
+		background: var(--alternative-color);
+		background: linear-gradient(270deg, var(--alternative-color) 20%, var(--secondary-color) 100%);
+	}
+	.li_footer {
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		padding: 0 var(--spacing-4) var(--spacing-4);
+	}
+	img {
+		width: 65%;
+		border-radius: var(--spacing-2);
+		margin: var(--spacing-1) 0;
+	}
+	a {
+		display: block;
+		font-family: var(--font-family);
+		font-size: var(--font-size-1);
+		font-weight: 500;
+	}
+
+	@media only screen and (max-width: 1140px) and (min-width: 640px) {
+		li {
+			display: flex;
+			width: 100%;
+			margin: var(--spacing-3) 0;
+			padding: var(--spacing-4);
+			> div {
+				display: inline-block;
+				width: 65%;
+			}
+		}
+		.li_footer {
+			position: relative;
+			width: 30%;
+			text-align: right;
+			bottom: unset;
+			left: unset;
+			padding: var(--spacing-4) 0 0 0;
+		}
+		img {
+			width: 90%;
+			margin: 0;
+		}
+	}
+
+	@media only screen and (max-width: 640px) {
+		li {
+			width: 100%;
+			margin: var(--spacing-3) 0;
+			padding: var(--spacing-4) var(--spacing-4) 54% var(--spacing-4);
+		}
+	}
+</style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import type { LayoutData } from './$types';
+	import DemandCard from '$lib/Components/DemandCard.svelte';
+ 	import type { LayoutData } from './$types';
 	let { data }: { data: LayoutData } = $props();
+	console.log(data.data)
 </script>
 
 <svelte:head>
@@ -16,16 +18,7 @@
 	<nav>
 		<ul>
 			{#each data.demands as demand, index}
-				<li>
-					<div>
-						<h3>Demand {index + 1}</h3>
-						<p>{demand.title}</p>
-					</div>
-					<div class="li_footer">
-						<img alt="A placeholder" src="/placeholder.gif" />
-						<a href="/{demand.id}">check it out ↗</a>
-					</div>
-				</li>
+				<DemandCard demand={demand} number={index + 1}/>
 			{/each}
 		</ul>
 	</nav>
@@ -38,68 +31,16 @@
 		list-style: none;
 		justify-content: space-between;
 	}
-	li {
-		display: inline-block;
-		width: 30%;
-		border-radius: var(--spacing-3);
-		padding: var(--spacing-4) var(--spacing-4) 18% var(--spacing-4);
-		background: var(--alternative-color);
-		background: linear-gradient(270deg, var(--alternative-color) 20%, var(--secondary-color) 100%);
-	}
-	.li_footer {
-		position: absolute;
-		left: 0;
-		bottom: 0;
-		padding: 0 var(--spacing-4) var(--spacing-4);
-	}
-	img {
-		width: 65%;
-		border-radius: var(--spacing-2);
-		margin: var(--spacing-1) 0;
-	}
-	a {
-		display: block;
-		font-family: var(--font-family);
-		font-size: var(--font-size-1);
-		font-weight: 500;
-	}
 
 	@media only screen and (max-width: 1140px) and (min-width: 640px) {
 		ul {
 			display: block;
-		}
-		li {
-			display: flex;
-			width: 100%;
-			margin: var(--spacing-3) 0;
-			padding: var(--spacing-4);
-			> div {
-				display: inline-block;
-				width: 65%;
-			}
-		}
-		.li_footer {
-			position: relative;
-			width: 30%;
-			text-align: right;
-			bottom: unset;
-			left: unset;
-			padding: var(--spacing-4) 0 0 0;
-		}
-		img {
-			width: 90%;
-			margin: 0;
 		}
 	}
 
 	@media only screen and (max-width: 640px) {
 		ul {
 			flex-direction: column;
-		}
-		li {
-			width: 100%;
-			margin: var(--spacing-3) 0;
-			padding: var(--spacing-4) var(--spacing-4) 54% var(--spacing-4);
 		}
 	}
 </style>
