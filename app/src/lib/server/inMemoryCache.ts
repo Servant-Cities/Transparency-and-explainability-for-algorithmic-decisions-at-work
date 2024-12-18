341bca700ec0cf89d1aaa7895ae42707771a2af6
Simon Mulquin <simon.mulquin@gmail.com>
2024-12-18 17:07:05 +0000

    feat: ✨ Persists cache in a json file

--- app/src/lib/server/inMemoryCache.ts
+++ app/src/lib/server/inMemoryCache.ts
@@ -1,4 +1,14 @@
-const memoryCache: Record<string, any> = {};
+import fs from 'node:fs';
+import { env } from '$env/dynamic/private';
+const { PERSIST_CACHE_TIME = '21600000' } = env;
+const CACHE_FILE_PATH = './cache.json';
+
+const getPersistedCache = () => {
+	const json = fs.readFileSync(CACHE_FILE_PATH, 'utf8');
+	return JSON.parse(json);
+};
+
+const memoryCache: Record<string, any> = getPersistedCache();
 
 export default async function inMemoryCache<Params, Return>(
 	query: (args?: Params) => Promise<Return>,
@@ -19,3 +29,17 @@ export default async function inMemoryCache<Params, Return>(
 
 	return handler;
 }
+
+// Persists cache every (PERSIST_CACHE_TIME) milliseconds
+setInterval(
+	() => {
+		fs.writeFile('CACHE_FILE_PATH', JSON.stringify(memoryCache), (err) => {
+			if (err) {
+				console.warn('The following errors happened while trying to write the cache: ', err);
+			} else {
+				console.info('Cache has been successfully persisted in the cache.json file');
+			}
+		});
+	},
+	parseInt(PERSIST_CACHE_TIME, 10)
+);
