f4a56eef7ab8afd87d9acab370f25dc6dae65d82
Simon Mulquin <simon.mulquin@gmail.com>
2025-01-13 22:38:50 +0100

    feat: ✨ Adds condition to overide favicons in metatags with a custom one

--- app/src/lib/utils/formatMetatagProperty.ts
+++ app/src/lib/utils/formatMetatagProperty.ts
@@ -1,9 +1,13 @@
 import { PUBLIC_CAMPAIGN_ORGANIZER_URL } from '$env/static/public';
 
 const formatMetatagProperty = ([key, value]: [string, string]) => {
-    if (value.startsWith('/sites')) {
-        return `${key}="${PUBLIC_CAMPAIGN_ORGANIZER_URL}${value}"`
-    }
+	const pathArray = value.split('/');
+	if (pathArray.includes('fav')) {
+		return `${key}="${PUBLIC_CAMPAIGN_ORGANIZER_URL}/${pathArray[pathArray.length]}"`;
+	}
+	if (value.startsWith('/sites')) {
+		return `${key}="${PUBLIC_CAMPAIGN_ORGANIZER_URL}${value}"`;
+	}
 	return `${key}="${value}"`;
 };
 
